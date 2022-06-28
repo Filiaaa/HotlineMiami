8bd8f89e4e82f598ac0f71e242a7fabe4390a923
S-MrV <egor.smirnov.n@gmail.com>
2022-06-28 14:39:29 +0300

    рабочие (ненастроенные) Мка, Узи, дробовик

--- Assets/Scripts/Weapon/FireWeapon/FireWeapon.cs
+++ Assets/Scripts/Weapon/FireWeapon/FireWeapon.cs
@@ -5,36 +5,48 @@ using UnityEngine;
 public class FireWeapon : Weapon
 {
 	public GameObject bullet;
-	public float attackTime;
+	public float attackTime, beforeNextBulletTime;
 	public float minVarience;
 	public float maxVarience;
-	public int force;
+	public int force, bulletsInQueue, bulletNumber;
 	public	bool canAttack = true;
 	Vector3 navigation;
 
+	public override void Attack () {
 
-	public override void Attack()
-	{
-
-		if (canAttack)
-		{
+		if (canAttack) {
 			var bullet_ = Instantiate(bullet, transform.position, Quaternion.identity, null);
-			navigation = new Vector3(transform.right.x + Random.RandomRange(minVarience, maxVarience), transform.right.y + Random.RandomRange(minVarience, maxVarience), transform.right.z);
+			navigation = new Vector3 (transform.right.x + Random.Range (minVarience, maxVarience), transform.right.y + Random.Range (minVarience, maxVarience), transform.right.z);
 			bullet_.GetComponent<Rigidbody2D>().AddForce(navigation * force/*, ForceMode2D.Impulse*/);
 			/*			bullet.GetComponent<Bullet>().navigation = transform.right;*/
-			canAttack = false;
-			StartCoroutine(TimeBeforeAttack());
+
+			if (bulletNumber < bulletsInQueue - 1) {
+				StartCoroutine (TimeBeforeNextBullet ());
+				bulletNumber++;
+			} else {
+				StartCoroutine(TimeBeforeAttack ());
+				bulletNumber = 0;
+				canAttack = false;
+			}
 		}
 
 	}
 
 
-	IEnumerator TimeBeforeAttack()
-	{
+	IEnumerator TimeBeforeAttack () {
 
 		yield return new WaitForSeconds(attackTime);
 		/*        GetComponent<Animator>().SetBool("isAttacking", false);*/
 		canAttack = true;
 
 	}
+
+
+	IEnumerator TimeBeforeNextBullet () {
+
+		yield return new WaitForSeconds(beforeNextBulletTime);
+		/*        GetComponent<Animator>().SetBool("isAttacking", false);*/
+
+	}
+
 }
