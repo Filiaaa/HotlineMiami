b745a0453f8f75261d9667c4f390835607c98728
Filiaaa <97357614+Filiaaa@users.noreply.github.com>
2022-09-14 21:22:49 +0300

    шрифт, немного интерфейса

--- Assets/Scripts/SelectButton.cs
+++ Assets/Scripts/SelectButton.cs
@@ -5,22 +5,49 @@ using UnityEngine.UI;
 
 public class SelectButton : MonoBehaviour
 {
+    public GameObject movingSelectingWindow;
     public Button[] buttons;
     private int currentButton = 0;
+    public float movingSpeed, scaleSpeed;
+
+    
+
 
     void Update()
     {
-        if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && currentButton != buttons.Length - 1)
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
         {
-            currentButton++;
-            buttons[currentButton].Select();
             buttons[currentButton].onClick.Invoke();
         }
-        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && currentButton != 0)
+        if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) )
         {
+            StopAllCoroutines();
+            currentButton++;
+            currentButton %= buttons.Length;
+            StartCoroutine(SelectBtn(new Vector3(buttons[currentButton].transform.position.x - 3, buttons[currentButton].transform.position.y, 1), new Vector3(buttons[currentButton].transform.localScale.x + 0.03f, buttons[currentButton].transform.localScale.y + 0.2f)));
+/*            buttons[currentButton].onClick.Invoke();*/
+        }
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)))
+        {
+            StopAllCoroutines();
             currentButton--;
-            buttons[currentButton].Select();
-            buttons[currentButton].onClick.Invoke();
+            if(currentButton == -1)
+            {
+                currentButton = buttons.Length - 1;
+            }
+            StartCoroutine(SelectBtn(new Vector3(buttons[currentButton].transform.position.x - 3, buttons[currentButton].transform.position.y, 1), new Vector3(buttons[currentButton].transform.localScale.x + 0.03f, buttons[currentButton].transform.localScale.y + 0.2f)));
+            /*            buttons[currentButton].onClick.Invoke();*/
+        }
+    }
+
+    IEnumerator SelectBtn(Vector3 targetPos, Vector3 targetScale)
+    {
+        while (Mathf.Abs(movingSelectingWindow.transform.position.y - targetPos.y) > Mathf.Epsilon || Mathf.Abs(movingSelectingWindow.transform.localScale.x - targetScale.x) > Mathf.Epsilon)
+        {
+            print("a");
+            movingSelectingWindow.transform.position = Vector3.Lerp(movingSelectingWindow.transform.position, targetPos, movingSpeed);
+            movingSelectingWindow.transform.localScale = Vector3.Lerp(movingSelectingWindow.transform.localScale, targetScale, scaleSpeed);
+            yield return new WaitForEndOfFrame();
         }
     }
 }
